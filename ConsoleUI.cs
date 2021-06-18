4b87c77f50d2b01b63787e5d1b466f0e81d0a3f8
Christoper <c.blake@hotmail.se>
2021-06-18 09:41:17 +0200

    Add project files.

---
+++ ConsoleUI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Flow_Control
+{
+    internal class ConsoleUI
+    {
+        public ConsoleUI()
+        {
+        }
+
+        public void Print(string message) // Public för interface
+        {
+            Console.WriteLine(message);
+        }
+
+        internal string GetInput()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Flow_Control
+{
+    class Program
+    
+    {
+        static ConsoleUI ui = new ConsoleUI();
+        static void Main(string[] args)
+        {
+            // MAIN MENU
+            do
+            {
+                ShowMainMenu();
+                UserInput();
+
+            } while (true);
+            
+        }
+
+        private static void UserInput()
+        {
+            switch (ui.GetInput())
+            {
+                case "0":
+                    break; // Bryter programmets första While Loop
+                default:
+                    ui.Print("Error");
+                    break;
+            }
+        }
+
+        private static void ShowMainMenu()
+        {
+            ui.Print("Type 0 to close the program");
+            ui.Print("Type other things");
+        }
+
+
+
+    }
+}
